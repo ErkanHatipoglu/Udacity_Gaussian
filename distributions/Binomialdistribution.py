74b4b92d069a7d6e2f286faafe577cef1532ee90
Erkan Hatipoglu <e109261@gmail.com>
2021-03-09 14:48:04 +0300

    Fix Typo

--- distributions/Binomialdistribution.py
+++ distributions/Binomialdistribution.py
@@ -12,7 +12,7 @@ class Binomial(Distribution):
     Attributes:
         mean (float) representing the mean value of the distribution
         stdev (float) representing the standard deviation of the distribution
-        data_list (list of floats) a list of floats to be extracted from the data file
+        data (list of floats) a list of floats to be extracted from the data file
         p (float) representing the probability of an event occurring
                 
     """
